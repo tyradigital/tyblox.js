88c8f9eac67c37c6935f55b727fdf7a6ae2e5c77
F1sxher <me@fisherdev.tk>
2021-11-28 10:38:08 -0500

    init(typings): start index typings

---
+++ typings/index.d.ts
@@ -0,0 +1,38 @@
+export type AvatarType = "headshot" | "avatarBust" | "avatarThumbnail";
+
+export class Client {}
+
+export class User {
+  private _cookie: string | null;
+  private _previousNames: string[] | null;
+  private _avatar: {
+    headshot: string | null;
+    avatarBust: string | null;
+    avatarThumbnail: string | null;
+  };
+  public readonly userId: string | number;
+  public readonly username: string;
+  public readonly displayName: string | null;
+  public readonly hasPremium: boolean;
+  public readonly banned: boolean;
+  public readonly description: string;
+  public limitedAccess(): boolean;
+  public avatarUrl(type: AvatarType): string;
+  public previousNames(): string[] | null;
+  public changeUsername(newUsername: string): string;
+  public changeDescription(newDescription: string): string;
+}
+
+export interface Header {
+  key: string;
+  value: string;
+}
+
+export interface RequestOptions {
+  baseUrl: string;
+  inUrlParam1?: string;
+  extendedUrl?: string;
+  inUrlParam2?: string;
+  headers?: Header[];
+  cookie?: string;
+}
