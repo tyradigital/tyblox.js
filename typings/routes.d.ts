334e70340a6249cc5e5a96ad2cd84da6fcd47948
F1sxher <72705185+F1sxher@users.noreply.github.com>
2021-12-01 17:43:57 -0500

    feat(typings): insert first route typing for the V1 mobileapi/userinfo

---
+++ typings/routes.d.ts
@@ -0,0 +1,8 @@
+export interface v1_users_mobileapi_userinfo {
+  UserId: string;
+  UserName: string;
+  RobuxBalance: number;
+  ThumbnailUrl: string;
+  IsAnyBuildersClub: boolean;
+  IsPremium: boolean;
+}
