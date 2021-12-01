1970297be63664f0898d24b057b04701eaf6b29c
F1sxher <72705185+F1sxher@users.noreply.github.com>
2021-12-01 17:39:27 -0500

    feat(example): first login example

--- examples/login.js
+++ examples/login.js
@@ -4,7 +4,9 @@ const { Client } = require("../src/index");
 const client = new Client();
 
 client.on("ready", () => {
-  
-});
+    console.log("Ready!")
+    
+    console.log("Logged in as " + client.user.username)
+}) 
 
 client.login(process.env.example_cookie);
