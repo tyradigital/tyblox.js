54509fb0bf2f0dd4037e4606a3b0284d242acd13
F1sxher <72705185+F1sxher@users.noreply.github.com>
2021-12-02 07:47:38 -0500

    add another interface for more user info stuff

--- typings/routes.d.ts
+++ typings/routes.d.ts
@@ -6,3 +6,13 @@ export interface v1_users_mobileapi_userinfo {
   IsAnyBuildersClub: boolean;
   IsPremium: boolean;
 }
+
+export interface v1_users_get_user_info_id {
+  description: string;
+  created: Date;
+  isBanned: boolean;
+  externalAppDisplayName: string;
+  id: string;
+  name: string;
+  displayName: string;
+}
