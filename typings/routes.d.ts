37582f7137f105712895be276b2b1ea51d80ab4a
F1sxher <72705185+F1sxher@users.noreply.github.com>
2021-12-09 07:36:41 -0500

    update ALL types to extend a base response interface w/ status and headers

--- typings/routes.d.ts
+++ typings/routes.d.ts
@@ -1,34 +1,92 @@
-export interface v1_users_mobileapi_userinfo {
-  UserID: string;
-  UserName: string;
-  RobuxBalance: number;
-  ThumbnailUrl: string;
-  IsAnyBuildersClub: boolean;
-  IsPremium: boolean;
+export interface response {
+  status: string;
+  headers: any;
 }
 
-export interface v1_users_get_user_info_id {
-  description: string;
-  created: Date;
-  isBanned: boolean;
-  externalAppDisplayName: string;
-  id: string;
-  name: string;
-  displayName: string;
+export interface v1_users_mobileapi_userinfo extends response {
+  data: {
+    UserID: string;
+    UserName: string;
+    RobuxBalance: number;
+    ThumbnailUrl: string;
+    IsAnyBuildersClub: boolean;
+    IsPremium: boolean;
+  };
 }
 
-export interface v1_users_username_history {
-  prevousPageCursor: null;
-  nextPageCursor: null;
-  data: Array<{ name: string }>;
+export interface v1_users_get_user_info_id extends response {
+  data: {
+    description: string;
+    created: Date;
+    isBanned: boolean;
+    externalAppDisplayName: string;
+    id: string;
+    name: string;
+    displayName: string;
+  };
 }
 
-export interface v1_thumbnails_users_avatar {
-  targetId: number;
-  state: string;
-  imageUrl: string;
+export interface v1_users_username_history extends response {
+  data: {
+    prevousPageCursor: null;
+    nextPageCursor: null;
+    data: Array<{ name: string }>;
+  };
 }
 
-export interface v1_thumbnails_user_avatars {
-  data: Array<v1_thumbnails_users_avatar>;
+export interface v1_thumbnails_users_avatar extends response {
+  data: {
+    targetId: number;
+    state: string;
+    imageUrl: string;
+  };
+}
+
+export interface v1_thumbnails_user_avatars extends response {
+  data: {
+    data: Array<v1_thumbnails_users_avatar>;
+  };
+}
+
+export interface v1_groups_info extends response {
+  data: {
+    id: number;
+    name: string;
+    description: string;
+    owner: {
+      buildersClubMembershipType: string;
+      userId: number;
+      username: string;
+      displayName: string;
+    };
+    shout: {
+      body: string;
+      poster: {
+        buildersClubMembershipType: string;
+        userId: number;
+        username: string;
+        displayName: string;
+      };
+      created: Date;
+      updated: Date;
+    };
+    memberCount: number;
+    isBuildersClubOnly: boolean;
+    publicEntryAllowed: boolean;
+    isLocked: true;
+  };
+}
+
+export interface v1_groups_roleset extends response {
+  data: {
+    id: string;
+    name: string;
+    rank: number;
+    memberCount: number;
+    description?: string;
+  };
+}
+
+export interface v1_groups_rolesets extends response {
+  data: { groupId: string; roles: Array<v1_groups_roleset> };
 }
