f26863050d1f436140f92c924f481d79e10eb321
F1sxher <72705185+F1sxher@users.noreply.github.com>
2021-12-09 07:30:22 -0500

    feat(request): new method types and more data added to responses

--- src/request.js
+++ src/request.js
@@ -28,6 +28,65 @@ exports.get = async (options) => {
     withCredentials: true,
     headers: finalHeaders,
   }).then((res) => {
-    return res.data;
+    return {code: res.status, headers: res.headers, data: res.data};
+  });
+};
+
+/**
+ * Do a `POST` request to an API, usually roblox.
+ * @param {import('../typings/index').RequestOptions} [options] The request options.
+ * @returns {Promise<any>}
+ * @example
+ * getUser.usingId('123456');
+ */
+ exports.post = async (options) => {
+  if (!options) throw new Error("OPTIONS REQUIRED")
+  if (!options.baseUrl) throw new Error("BASE URL REQUIRED")
+
+  let finalUrl =
+    options.baseUrl +
+    (options.inUrlParam1 || "") +
+    (options.extendedUrl || "") +
+    (options.inUrlParam2 || "");
+
+  let finalHeaders = {};
+  if (options.cookie) finalHeaders.Cookie = `.ROBLOSECURITY=${options.cookie};`;
+  if (options.headers) options.headers.forEach((header) => {
+    finalHeaders[header.key] = header.value;
+  });
+
+  return await axios.post(finalUrl, options.body || {}).then((res) => {
+    return {code: res.status, headers: res.headers, data: res.data};
+  });
+};
+
+/**
+ * Do a `PATCH` request to an API, usually roblox.
+ * @param {import('../typings/index').RequestOptions} [options] The request options.
+ * @returns {Promise<any>}
+ * @example
+ * getUser.usingId('123456');
+ */
+ exports.patch = async (options) => {
+  if (!options) throw new Error("OPTIONS REQUIRED")
+  if (!options.baseUrl) throw new Error("BASE URL REQUIRED")
+
+  let finalUrl =
+    options.baseUrl +
+    (options.inUrlParam1 || "") +
+    (options.extendedUrl || "") +
+    (options.inUrlParam2 || "");
+
+  let finalHeaders = {};
+  if (options.cookie) finalHeaders.Cookie = `.ROBLOSECURITY=${options.cookie};`;
+  if (options.headers) options.headers.forEach((header) => {
+    finalHeaders[header.key] = header.value;
+  });
+  console.log(finalUrl)
+  console.log(finalHeaders)
+  return await axios.patch(finalUrl, JSON.stringify(options.body) || {}).catch((err) => {
+    throw new Error(`REQUEST ERROR - ${err}`)
+  }).then((res) => {
+    return {code: res.status, headers: res.headers, data: res.data};
   });
 };
